308372f52fe42e265aa59a2a72e8711e59e4e423
Zadfahad <fahadalam693@gmail.com>
2022-06-02 17:54:31 +0200

    trying to center the main image

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -67,7 +67,7 @@
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner jumbotron-img">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="../img/h3-rev-img-6.png" alt="First slide">
+      <img class="d-block w-100 jumb-img" src="../img/h3-rev-img-6.png" alt="First slide">
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../img/h3-rev-img-2.png" alt="Second slide">
@@ -187,6 +187,9 @@ export default {
 }
 .jumbotron-img{
     width: 20%;
+    position: relative;
+    display: block;
+    
     
 }
 
